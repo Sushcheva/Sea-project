95c485c1fac9828e386cb6087d77a19730d2734b
Mahsa <Sushcheva2009@yandex.ru>
2022-01-06 11:24:18 +0300

    стандарт

--- main.py
+++ main.py
@@ -18,6 +18,83 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
+tile_images = {
+    'wall': load_image('box.png', None),
+    'empty': load_image('grass.png', None)
+}
+player_image = load_image('mar.png', None)
+
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.tile_type = tile_type
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        print(tile_type)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self, event):
+        if event == 1:
+            self.rect = self.rect.move(0, 50)
+            self.pos_y += 1
+        if event == 2:
+            self.rect = self.rect.move(0, -50)
+            self.pos_y -= 1
+        if event == 3:
+            self.rect = self.rect.move(50, 0)
+            self.pos_x += 1
+        if event == 4:
+            self.rect = self.rect.move(-50, 0)
+            self.pos_x -= 1
+
+
+def terminate():
+    fut()
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+
+    return new_player, x, y
+
+
+def load_level(filename):
+    filename = filename
+    # читаем уровень, убирая символы перевода строки
+    with open('18.txt', 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
 def ninja():
     pygame.init()
     horizontal_borders = pygame.sprite.Group()
