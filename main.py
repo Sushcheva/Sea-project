0c27383b1337dd57aa754a0486c70331a193d3b0
Kurganskaya <MaRia.KuRganskaya5@yandex.ru>
2022-01-13 17:18:10 +0300

    working base + zastavka

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, \
      QInputDialog, QLabel, QMessageBox
 from PyQt5 import QtGui  # для измениения шрифта
 from PyQt5.QtGui import QPixmap
-
+from PyQt5.QtWidgets import QPushButton, QApplication, QWidget, QInputDialog, QLabel
+from PyQt5 import QtGui
+from PyQt5.QtGui import QPixmap, QBrush, QPalette, QMovie, QPainter
 
 sp = []
 n = ' '
 a = ' '
 s = ' '
+
 
 class Example(QMainWindow):
     def __init__(self):
@@ -32,7 +35,7 @@ class Example(QMainWindow):
         self.image.setPixmap(self.pixmap)
         self.btn = QPushButton('Играть', self)
         self.btn.move(800, 700)
-        self.btn.clicked.connect(self.open_seven_form)
+        self.btn.clicked.connect(self.open_sev_form)
         self.btn.setFont(QtGui.QFont("Times", 19, QtGui.QFont.Bold))
         self.btn.adjustSize()
         self.button_1 = QPushButton("Введи свои ФИО", self)
@@ -122,12 +125,91 @@ class Example(QMainWindow):
                 QMessageBox.about(self, 'АГА', d)
             con.commit()
 
-    def open_seven_form(self):
+    def open_sev_form(self):
+        self.thorth_form = Exa()
+        self.hide()
+        self.thorth_form.show()
+        self.thorth_form.showFullScreen()
+
+class Exa(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.movie = QMovie("gif.gif")
+        self.movie.frameChanged.connect(self.repaint)
+        self.movie.start()
+        self.image = QLabel(self)
+        self.label = QLabel(self)
+        self.setGeometry(700, 700, 700, 700)
+        self.setWindowTitle('Игра')
+        self.label.setText('Добро пожаловать! Выбирай игру:')
+        self.label.setFont(QtGui.QFont("Gabriola", 46, QtGui.QFont.Black))
+        self.label.adjustSize()
+        self.label.move(450, 150)
+        self.image.move(40, 40)
+        self.image.resize(600, 610)
+        self.button_1 = QPushButton("Фруктовый ниндзя", self)
+        self.button_1.move(400, 450)
+        self.button_1.setStyleSheet('background: rgb(153, 0, 255);')
+        self.button_1.setFont(QtGui.QFont("Gabriola", 30))
+        self.button_1.adjustSize()
+        self.button_1.clicked.connect(self.run1)
+        self.button_2 = QPushButton("Волшебный лабиринт", self)
+        self.button_2.move(1120, 450)
+        self.button_2.setStyleSheet('background: rgb(153, 0, 255);')
+        self.button_2.setFont(QtGui.QFont("Gabriola", 30))
+        self.button_2.adjustSize()
+        self.button_2.clicked.connect(self.run2)
+        self.button_2.hide()
+        self.label1 = QLabel(self)
+        self.label1.move(900, 300)
+        self.label2 = QLabel(self)
+        self.label2.move(900, 300)
+        self.button_2.show()
+
+    def paintEvent(self, event):
+        currentFrame = self.movie.currentPixmap()
+        frameRect = currentFrame.rect()
+        frameRect.moveCenter(self.rect().center())
+        if frameRect.intersects(event.rect()):
+            painter = QPainter(self)
+            painter.drawPixmap(frameRect.left(), frameRect.top(), currentFrame)
+
+    def run1(self):
+        ninlvl, ok_pressed = QInputDialog.getItem(
+            self, "Сложность", "Выбери уровень:",
+            ('для новичка', 'средний', 'сложный'), 1, False)
+        if ok_pressed:
+            self.label1.setText(ninlvl)
+            self.label1.setFont(QtGui.QFont("Gabriola", 36, QtGui.QFont.Bold))
+            self.label1.adjustSize()
+            self.button_1.hide()
+            self.button_2.hide()
+            self.open_second_form()
+
+    def run2(self):
+        lablvl, ok_pressed = QInputDialog.getItem(
+            self, "Карта", "Выбери сложность карты:",
+            ("простая", "средняя", "сложная"),
+            1, False)
+        if ok_pressed:
+            self.label2.setText(lablvl)
+            self.label2.setFont(QtGui.QFont("Gabriola", 36, QtGui.QFont.Bold))
+            self.label2.adjustSize()
+            self.button_2.hide()
+            self.button_1.hide()
+            self.open_second_form()
+
+
+    def open_second_form(self):
         pass
 
 
+def e(a, b, c):
+    sys.__excepthook__(a, b, c)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    sys.excepthook = e
     ex = Example()
     ex.show()
     sys.exit(app.exec())
