ecb87601a610273c3e567706c4f40ca4e01d17d0
Professional <MaRia.KuRganskaya5@yandex.ru>
2022-01-16 20:10:27 +0300

    for Mariaaaa

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ import sys
 import sqlite3
 import pygame
 import random
-from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, \
-     QInputDialog, QLabel, QMessageBox
-from PyQt5 import QtGui  # для измениения шрифта
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QPushButton, QApplication, QWidget, QInputDialog, QLabel
-from PyQt5 import QtGui
-from PyQt5.QtGui import QPixmap, QBrush, QPalette, QMovie, QPainter
 import os
 from random import sample, randrange, choice
+from PyQt5.QtWidgets import QMainWindow,  QMessageBox, QPushButton, QApplication, QWidget, QInputDialog, QLabel
+from PyQt5 import QtGui
+from PyQt5.QtGui import QPixmap, QBrush, QPalette, QMovie, QPainter
+
 
 tile_width = tile_height = 50
 pygame.font.init()
@@ -26,24 +23,8 @@ strix_group = pygame.sprite.Group()
 player = None
 screen_rect = (0, 0, 500, 500)
 
-
-def over_game():
-    pygame.init()
-    size1 = 1000, 1000
-    screen1 = pygame.display.set_mode(size1)
-    clock1 = pygame.time.Clock()
-    fon1 = pygame.transform.scale(load_image('game over.png'), (500, 500))
-    exit(screen)
-    quit(screen1)
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.type == pygame.K_ESCAPE:
-                     terminate()
-
+st = 0
+d = ['en.png', 'enn.png', 'ennn.png']
 
 
 def load_level(filename):
@@ -164,39 +145,55 @@ def over_game():
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     running = True
+    font = pygame.font.Font(None, 30)
+    text_coord = 400
+    string_rendered = font.render(str('Вы набрали ' + str(st) + ' баллов'), 1, pygame.Color('white'))
+    intro_rect = string_rendered.get_rect()
+    intro_rect = string_rendered.get_rect()
+    intro_rect.top = text_coord
+    intro_rect.x = 25
     fon = pygame.transform.scale(load_image('game over.png'), (900, 800))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                terminate()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                terminate()
         screen.blit(fon, (0, 0))
         clock.tick(8)
         pygame.display.flip()
+        fon.blit(string_rendered,intro_rect)
+
 
 def win_game():
     pygame.init()
     size = 900, 800
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
+    con = sqlite3.connect("base55.db")
+    cur = con.cursor()
+    info = cur.execute(f"SELECT stars FROM person WHERE name =='{n} AND age == '{a}'")
+    if info.fetchone() is None:
+        f"INSERT INTO person(stars)"
+        f" VALUES('{st}')"
+    else:
+        cur.execute(f'UPDATE person SET stars=? WHERE name=? AND age=?', (st, n, a))
+    con.commit()
     running = True
     fon = pygame.transform.scale(load_image('win.png'), (900, 800))
     font = pygame.font.Font(None, 30)
-    text_coord = 300
-    string_rendered = font.render(str('Вы набрали '+str(st)+' баллов'), 1, pygame.Color('white'))
+    text_coord = 400
+    string_rendered = font.render(str('Вы набрали ' + str(st) + ' баллов'), 1, pygame.Color('white'))
     intro_rect = string_rendered.get_rect()
     intro_rect = string_rendered.get_rect()
     intro_rect.top = text_coord
-    intro_rect.x = 910
+    intro_rect.x = 600
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                terminate()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                terminate()
         screen.blit(fon, (0, 0))
         fon.blit(string_rendered, intro_rect)
         clock.tick(8)
@@ -228,7 +225,7 @@ class AnimatedSprite(pygame.sprite.Sprite):
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('data/', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
@@ -236,8 +233,6 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
-st = 0
-d = ['en.png', 'enn.png', 'ennn.png']
 
 
 tile_width = tile_height = 50
@@ -537,8 +532,6 @@ def ninja():
         pygame.display.flip()
         clock.tick(100)
 
-
-
 sp = []
 n = ' '
 a = ' '
@@ -640,7 +633,7 @@ class Example(QMainWindow):
             s = state
             print(s)
             print(sp)
-            con = sqlite3.connect('base.db')
+            con = sqlite3.connect('base55.db')
             cur = con.cursor()
             print(cur)
             rez = cur.execute(f'SELECT * FROM person WHERE name=? AND age=? AND state=?', (n, a, s))
@@ -830,3 +823,4 @@ if __name__ == '__main__':
     ex = Example()
     ex.show()
     sys.exit(app.exec())
+
